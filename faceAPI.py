3fe10754d6b4df66afe3ace0a5b4ddc4f106fcd4
alexdarch <ad806@cam.ac.uk>
2019-01-20 01:22:51 +0000

    faceAPI class

--- faceAPI.py
+++ faceAPI.py
@@ -5,129 +5,106 @@ Created on Sat Jan 19 16:26:22 2019
 @author: benwi
 """
 import requests
-from IPython.display import HTML
-from PIL import Image
-from io import BytesIO
-import numpy as np
+import pandas as pd
+# from IPython.display import HTML
+# from PIL import Image
+# from io import BytesIO
 
-subscription_key = '6d1691bf159940679209d2d734d2e2e0'
-assert subscription_key
 
-faceListId = '19012019facelist'
+class FaceAPI:
 
-image_url= "https://how-old.net/Images/faces2/main001.jpg"
+    subscription_key = '6d1691bf159940679209d2d734d2e2e0'
 
-def findFaceInImage(image_data):
-    '''Find faces in an image'''
-    faceDetect_api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/detect'
-    
-    headers = { 'Ocp-Apim-Subscription-Key': subscription_key,
-               'content-type': 'application/octet-stream'
-               }
-        
-    params = {
-        'returnFaceId': 'true',
-        'returnFaceLandmarks': 'true',
-        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
-    }
-    
-    response = requests.post(faceDetect_api_url, params=params, headers=headers, data=image_data)
-    faces = response.json()
-    print(faces)
-    return faces
-    
-def createNewFaceList(facelistname):
-    '''Create new FaceList'''
-    faceListName = facelistname
-    faceListCreate_api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/facelists/' + faceListName
-    
-    headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key,
-    }
-    
-    params = {
-        'faceListId': faceListName,
-    }
-    
-    response = requests.put(faceListCreate_api_url, params=params, headers=headers, json={"name": faceListName})
-    print(response.json())
-    
-def allocateToFaceList(image_data, face, facelistname):
-    '''Allocate new faces to FaceList'''
-    faceListName = facelistname
-    newFaceToFaceList_api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/facelists/'+faceListName+'/persistedFaces'
-    
-    image_file = BytesIO(requests.get(image_url).content)
-    image = Image.open(image_file)
-    width, height = image.size
-    
-    faceRectangle = face['faceRectangle']
-    left = np.round((faceRectangle['left']/width)*100)
-    top = np.round((faceRectangle['top']/height)*100)
-    width = np.round((faceRectangle['width']/width)*100)
-    height = np.round((faceRectangle['height']/height)*100)
-#    left = int(faceRectangle['left'] *100)
-#    top = int(faceRectangle['top'] *100)
-#    width = int(faceRectangle['width'] *100)
-#    height = int(faceRectangle['height'] *100)
-    
-    targetFace = 'targetFace='+str(left)+','+str(top)+','+str(width)+','+str(height)
-    print(targetFace)
-    
-    headers = { 'Ocp-Apim-Subscription-Key': subscription_key,
-               'content-type': 'application/octet-stream'
-               }
-        
-    params = {
-        'faceListId': faceListName,
-        'targetFace': targetFace,
-    }
-    
-    response = requests.post(newFaceToFaceList_api_url, params=params, headers=headers, data=image_data)
-    print(response.json())
-    
-def findSimilarFaces(face, facelistname):    
-    '''Face similarity'''
-    faceListName = facelistname
-    faceId = face['faceId']    
-    
-    faceSimilarity_api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/findsimilars'
-    
-    headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
-    
-    response = requests.post(faceSimilarity_api_url, headers=headers, json={'faceId': faceId, 'faceListId': faceListName, 'mode': 'matchFace', "maxNumOfCandidatesReturned": 30})
-    similarFaces = response.json() # Returns persistedFaceId and confidence for each response
-    print(similarFaces)
-    
-    return similarFaces
-    
-def getFaceList(facelistname):
-    '''See what's in the FaceList'''
-    faceListName = facelistname
-    
-    getFaceList_api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/facelists/'+faceListName
-    headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
-        
-    params = {
-        'faceListId': faceListName,
-    }
-    
-    response = requests.get(getFaceList_api_url, params=params, headers=headers)
-    
-    print(response.json())
-    
-    return response.json()
-    
-#faces = findFaceInImage(image_url)
-#createNewFaceList(faceListId)
+    def __init__(self, face_list_id, image_url):
+        self.api_url = 'https://northeurope.api.cognitive.microsoft.com/face/v1.0'
+        self.face_list_id = face_list_id
+        self.image_url = image_url
+
+    def create_new_face_list(self):
+        """Create new FaceList"""
+        face_list_create_api_url = self.api_url + '/facelists/' + self.face_list_id
+        response = requests.put(face_list_create_api_url,
+                                params={'faceListId': self.face_list_id},
+                                headers={'Ocp-Apim-Subscription-Key': FaceAPI.subscription_key},
+                                json={"name": self.face_list_id})
+        print(response.json())
+
+    def find_face_in_image(self, path):
+        """Find faces in an image"""
+        image_binary = open(path, "rb")
+        face_detect_api_url = self.api_url + '/detect'
+        headers = {'Ocp-Apim-Subscription-Key': FaceAPI.subscription_key, 'content-type': 'application/octet-stream'}
+        params = {'returnFaceId': 'true',
+                  'returnFaceLandmarks': 'true',
+                  'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,'
+                                          'hair,makeup,occlusion,accessories,blur,exposure,noise'
+                  }
+        response = requests.post(face_detect_api_url, params=params, headers=headers, data=image_binary)
+        faces_features = response.json()
+        print(faces_features)
+        return faces_features
+
+    def allocate_to_face_list(self, path):
+        """Allocate new faces to FaceList"""
+        image_binary = open(path, "rb")
+
+        new_face_to_face_list_api_url = self.api_url + '/facelists/' + self.face_list_id + '/persistedFaces'
+
+        headers = {'Ocp-Apim-Subscription-Key': FaceAPI.subscription_key,
+                   'content-type': 'application/octet-stream'
+                   }
+        params = {'faceListId': self.face_list_id}
+        response = requests.post(new_face_to_face_list_api_url,
+                                 params=params,
+                                 headers=headers,
+                                 data=image_binary)
+
+        print("response.json()", response.json())
+
+    def find_similar_faces(self, face_features_dict)->list:
+        """Face similarity"""
+
+        face_id = face_features_dict['faceId']
+        face_similarity_api_url = self.api_url + '/findsimilars'
+        headers = {'Ocp-Apim-Subscription-Key': FaceAPI.subscription_key}
+        response = requests.post(face_similarity_api_url,
+                                 headers=headers,
+                                 json={'faceId': face_id,
+                                       'faceListId': self.face_list_id,
+                                       'mode': 'matchFace',
+                                       'maxNumOfCandidatesReturned': 30}
+                                 )
+        similar_faces = response.json()  # Returns persistedFaceId and confidence for each response
+        print("similar_faces ", similar_faces)
+
+        return similar_faces    # list of dictionaries of face characteristics
+
+    def get_face_list(self)-> list:
+        """See what's in the FaceList"""
+
+        get_face_list_api_url = self.api_url + '/facelists/' + self.face_list_id
+        headers = {'Ocp-Apim-Subscription-Key': FaceAPI.subscription_key}
+        params = {'faceListId': self.face_list_id}
+        response = requests.get(get_face_list_api_url,
+                                params=params,
+                                headers=headers)
+
+        print("response.json()2: ", response.json())
+        return response.json()
+
+
+# pass in some test ids and urls
+face_api = FaceAPI(face_list_id='19012019facelist', image_url="https://how-old.net/Images/faces2/main00.jpg")
+
+face_api.create_new_face_list()
+image_faces = face_api.find_face_in_image(path='test_img.jpg')
+
+for face_dict in image_faces:
+    face_api.allocate_to_face_list('test_img.jpg')
+    face_api.find_similar_faces(face_dict)
+
+face_api.get_face_list()
+
+
 #
-#for face in faces:
-#    allocateToFaceList(image_url, face, faceListId)
-#    findSimilarFaces(face, faceListId)
-#    
-#getFaceList(faceListId)
-    
-image_path = 'test_img.jpg'
-image_data = open(image_path, "rb")
-
-faces = findFaceInImage(image_data)
+# faces = find_face_in_image(image_data)
