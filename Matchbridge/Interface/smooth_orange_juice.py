02c6591dddc866992e594b3b68225a7fbadcf51f
alexdarch <ad806@cam.ac.uk>
2019-01-20 11:59:53 +0000

    submission

--- Matchbridge/Interface/smooth_orange_juice.py
+++ Matchbridge/Interface/smooth_orange_juice.py
@@ -1,5 +1,5 @@
 import numpy as np
-from faceAPI import FaceAPI
+from Interface.faceAPI import FaceAPI
 import pandas as pd
 import os
 
@@ -16,25 +16,27 @@ class SmoothOrangeJuice():
         # initialise similarity average container
         self.similarity_average = pd.DataFrame(data={'PersistedFaceId':[], 'ImagePath':[], 'Confidence':[],})
         # main image dataset
-        self.dataset_dir = './images/main'
+        self.dataset_dir = './static/images/main'
         self.dataset = self.get_dataset(init=False)
         # initial image dataset
-        self.init_dir = './images/initial'
+        self.init_dir = './static/images/initial'
         self.initial_size = 30
         self.image_set = self.get_dataset(init=True)
         
         # counter for number of images that have been considered
         self.count = 0 
         # initialise dictionary from filename to faceid
-        self.file_to_face = {}       
+        self.file_to_face = {}
 
     def get_dataset(self, init=False):
         # Get list of images in directory
         if init is False:
+            print('getting dataset for main')
             files = os.listdir(self.dataset_dir)
             root = self.dataset_dir
 
-        if init is True: 
+        if init is True:
+            print('getting dataset for initialisation')
             files = os.listdir(self.init_dir)
             root = self.init_dir
         
@@ -46,16 +48,18 @@ class SmoothOrangeJuice():
             path = root + '/' + f
             # load the faces in the image to facelist for main dataset
             if init is False:
+                if i is 98:
+                    print('ahah')
                 persisted_face_id = self.fa.allocate_to_face_list(path)
             # initialise the whole dataset with pfaceid, path and confidence
                 to_append = pd.DataFrame(
-                    [[persisted_face_id['persistedFaceId'], f, 0.0]], 
+                    [[persisted_face_id['persistedFaceId'], f, 0.0]],
                     columns = ['PersistedFaceId','ImagePath','Confidence']
                     )
                 self.similarity_average = (
                     self.similarity_average.append(to_append, ignore_index=True)
                     )
-                print(self.similarity_average)
+            print(self.similarity_average.iloc[i])
         return container
 
     def update_similarity_average(self, image, response, init=False):
@@ -114,35 +118,32 @@ class SmoothOrangeJuice():
         print('receive input')
         return a
 
-    def initialisation_run(self):
+    def initialisation_run(self, response):
         # array of images to use for initial average calculation
-        for i in range(len(self.image_set)):
-            image = self.image_set[i]
+        if len(self.image_set) is not self.count:
+            image = self.image_set[self.count]
             # receive a +1 for YES, -1 for NO from user response
-            response = self.send_image_to_console(image)
             self.update_similarity_average(image, response, init=True)
+
+            return self.image_set[self.count+1]
         
         print("Initialisation finished")
         # self.Running is set true/false
         return None
 
-    def main_run(self):    
-        while len(self.similarity_average) > 0: 
+    def main_run(self, response):
+        if len(self.similarity_average) > 0:
             # get best image from the dataset and remove the used image
             image = self.get_best_image()
             # Clean up our list of faces to prevent reusing ones used before
             self.similarity_average = self.similarity_average.loc[
                 self.similarity_average.Confidence!=self.similarity_average.Confidence.max()
                 ]
-            response = self.send_image_to_console(image)
+            # response = self.send_image_to_console(image)
             self.fa.get_face_list()
             self.update_similarity_average(image, response)
+            return image
             
         print("Ran out of images to give")
         self.fa.delete_facelist()
         return None
-            
-if __name__ == "__main__":
-    prog = SmoothOrangeJuice()
-    prog.initialisation_run()
-    prog.main_run()
--- Matchbridge/application.py
+++ Matchbridge/application.py
@@ -3,7 +3,7 @@ import numpy as np
 sys.path.insert(0,'./Interface')
 
 from Interface import faceAPI
-from smooth_orange_juice import SmoothOrangeJuice
+from Interface.smooth_orange_juice import SmoothOrangeJuice
 from flask import Flask, render_template, request, flash
 from flask_wtf import FlaskForm
 app = Flask(__name__)
@@ -41,11 +41,15 @@ image4 = {'image_src': ""}
 image5 = {'image_src': ""}
 image6 = {'image_src': ""}
 
+orange = SmoothOrangeJuice()
+orange.count = 0
 
 @app.route("/")
 @app.route("/home")
 def hello():
-	return render_template('home.html', image_data=image_data, image1=image1, image2=image2, image3=image3, image4=image4, image5=image5, image6=image6)
+    image_data = orange.image_set[orange.count]
+    orange.count += 1
+    return render_template('home.html', image_data=image_data, image1=image1, image2=image2, image3=image3, image4=image4, image5=image5, image6=image6)
 
 @app.route('/switchnegativeimage', methods=['GET', 'POST'])
 def switchnegative():
@@ -54,7 +58,22 @@ def switchnegative():
         # Alter image_data to next image we want to load in
         # Alter image 1-6 as top 6 ranked images
         # use top_images = dataframe.nlargest(6, 'Confidence')
-        image_data = {'image_src': "../static/images/image347.jpg"}
+        if orange.count < len(orange.image_set):
+            orange.initialisation_run()
+        else:
+            image = orange.main_run(response=-1.0)
+
+        image_location = "../static/images/" + image.ImagePath.iloc[0]
+        image_data = {'image_src': image_location}
+
+        image1 = {'image1': "../static/images/" + image.ImagePath.iloc[1]}
+        image2 = {'image2': "../static/images/" + image.ImagePath.iloc[2]}
+        image3 = {'image3': "../static/images/" + image.ImagePath.iloc[3]}
+        image4 = {'image4': "../static/images/" + image.ImagePath.iloc[4]}
+        image5 = {'image5': "../static/images/" + image.ImagePath.iloc[5]}
+        image6 = {'image6': "../static/images/" + image.ImagePath.iloc[6]}
+
+        orange.count += 1
         return render_template('home.html', image_data=image_data,image1=image1, image2=image2, image3=image3, image4=image4, image5=image5, image6=image6)
     
 @app.route('/switchpositiveimage', methods=['GET','POST'])
@@ -64,7 +83,22 @@ def switchpositiveimage():
         # Alter image_data to next image we want to load in
         # Alter image 1-6 as top 6 ranked images
         # use top_images = dataframe.nlargest(6, 'Confidence')
-        image_data = {'image_src': "../static/images/image347.jpg"}
+        if orange.count < 38:
+            orange.initialisation_run()
+        else:
+            image = orange.main_run(response=1.0)
+
+        image_location = "../static/images/" + image.ImagePath.iloc[0]
+        image_data = {'image_src': image_location}
+
+        image1 = {'image1': "../static/images/" + image.ImagePath.iloc[1]}
+        image2 = {'image2': "../static/images/" + image.ImagePath.iloc[2]}
+        image3 = {'image3': "../static/images/" + image.ImagePath.iloc[3]}
+        image4 = {'image4': "../static/images/" + image.ImagePath.iloc[4]}
+        image5 = {'image5': "../static/images/" + image.ImagePath.iloc[5]}
+        image6 = {'image6': "../static/images/" + image.ImagePath.iloc[6]}
+
+        orange.count += 1
         return render_template('home.html', image_data=image_data,image1=image1, image2=image2, image3=image3, image4=image4, image5=image5, image6=image6)
 
 @app.route("/about")
